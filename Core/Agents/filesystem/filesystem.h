cfc31a7bc5ef1a51e7df78b03efb11e95f2c5a49
sharpyy <sharpyy@27a3ed10-190e-0410-b665-e8c4861faaa4>
2008-01-31 16:12:27 +0000

    [filesystem] first functions for logfile feature

--- Core/Agents/filesystem/filesystem.h
+++ Core/Agents/filesystem/filesystem.h
@@ -23,21 +23,47 @@
 #include "IBBWinAgent.h"
 
 #define MAX_TIME_BACKQUOTED_COMMAND		8000
-
+#define LOG_MAXDATA						10240
+#define MAX_SEEKDATA_POINT				6
 
 enum hash_type_t { NONE, MD5, SHA1 };
+
+// Struct used to save seek points 
+typedef struct				fs_logfile_seekdata_s {
+	DWORD					point[MAX_SEEKDATA_POINT];
+	bool					used;	// tell if the logfile is still monitored 
+									//and that seek data are still necessary
+}							fs_logfile_seekdata_t;
+
+// Struct used for log file monitoring
+typedef struct				fs_logfile_s {
+	std::string				path;
+	DWORD					maxdata;
+	std::list<std::string>	ignores;
+	std::list<std::string>	triggers;
+}							fs_logfile_t;
+
+// Struct used for linecount monitoring
+typedef struct				fs_linecount_s {
+	std::string				path;
+	std::list<std::string>	keywords;
+}							fs_linecount_t;
+
 
 // Struct used for file monitoring
 typedef struct			fs_file_s {
 	std::string			path;
 	enum hash_type_t	hashtype;
+	bool				logfile; // true if it is a logfile
 }						fs_file_t;
 
 class AgentFileSystem : public IBBWinAgent
 {
 	private :
-		std::list<fs_file_t>				m_files;
-		std::list<std::string>				m_dirs;
+		std::list<fs_file_t>							m_files;
+		std::list<std::string>							m_dirs;
+		std::list<fs_logfile_t>							m_logfiles;
+		std::map<std::string, fs_logfile_seekdata_t>	m_seekdata;
 
 	private :
 		IBBWinAgentManager 		& m_mgr;
@@ -51,6 +77,8 @@ class AgentFileSystem : public IBBWinAgent
 		void					ExecuteDirRule(const std::string & dir);
 		void					ListFiles(const std::string & path, std::stringstream & report, __int64 & size);
 		void					GetLinesFromCommand(const std::string & command, std::list<std::string> & list);
+		void					ExecuteLogFileRule(const fs_logfile_t & logfile);
+		bool					GetDataFromLogFile(const fs_logfile_t & logfile, std::stringstream data);
 
 	public :
 		AgentFileSystem(IBBWinAgentManager & mgr) ;
